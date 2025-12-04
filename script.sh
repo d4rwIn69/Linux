33f77f6b393d69c71d0dc71e96e9d6d10426807b
d4rwIn69 <daalcarrazto@uide.edu.ec>
2025-12-04 12:56:25 +0000

    hola commit

---
+++ script.sh
@@ -0,0 +1,11 @@
+#Crear y listar archivos 
+#echo imprime el texto que es "Hola Linux", el ">" Redirige la salida estandar y lo envia a un archivo y lo crea si no esta creado
+echo "Hola Linux" > archivo.txt
+#Concatena el contenido del archivo y mostrar en la salida estandar
+cat archivo.txt
+#Copiar archivos
+cp archivo.txt copia.txt
+#Listar archivos
+ls -l
+#Eliminar archivos
+rm archivo.txt
